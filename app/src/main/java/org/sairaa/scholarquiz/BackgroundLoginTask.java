70d70f917a52c386c22f84342eb569d38ed6a127
Praful Nayak <prafulnayak1988@gmail.com>
2018-04-10 12:36:56 +0530

    Auto Login Concept implemented.

--- app/src/main/java/org/sairaa/scholarquiz/BackgroundLoginTask.java
+++ app/src/main/java/org/sairaa/scholarquiz/BackgroundLoginTask.java
@@ -5,8 +5,10 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.EditText;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,18 +16,25 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLEncoder;
 
 public class BackgroundLoginTask extends AsyncTask<String, Void, String> {
+    private final String LOGIN_URL = "http://sairaa.org/ScholarQuiz/login.php";
     private final String REGISTER_URL = "http://sairaa.org/ScholarQuiz/register_test.php";
+
+    private SharedPreferenceConfig sharedPreferenceConfig;
+
     Context context;
     Activity activity;
     ProgressDialog progressDialog;
@@ -92,6 +101,51 @@ public class BackgroundLoginTask extends AsyncTask<String, Void, String> {
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else if(method.equals("login")){
+            try {
+                URL register_url = new URL(LOGIN_URL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) register_url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
+
+                String email =params[1];
+                String password = params[2];
+
+                String data = URLEncoder.encode("mail_id", "UTF-8")+"="+URLEncoder.encode(email,"UTF-8")+"&"+
+                        URLEncoder.encode("password", "UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line = "";
+
+                while((line = bufferedReader.readLine())!= null){
+                    stringBuilder.append(line+"\n");
+                    Log.i("background",line);
+                }
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -119,6 +173,17 @@ public class BackgroundLoginTask extends AsyncTask<String, Void, String> {
                 showDialog("Registration Successfull",message,code);
             }else if(code.equals("reg_false")){
                 showDialog("Registration Failed",message,code);
+            }else if(code.equals("login_true")){
+
+                Intent intent = new Intent(activity,LessonActivity.class);
+                intent.putExtra("message",message);
+                activity.startActivity(intent);
+                sharedPreferenceConfig = new SharedPreferenceConfig(activity);
+                sharedPreferenceConfig.writeLoginStatus(true);
+                activity.finish();
+
+            }else if(code.equals("login_false")){
+                showDialog("Error in Login",message,code);
             }
         } catch (JSONException e) {
             e.printStackTrace();
@@ -139,9 +204,26 @@ public class BackgroundLoginTask extends AsyncTask<String, Void, String> {
                 }
             });
 
-            AlertDialog alertDialog = alertBuilder.create();
-            alertDialog.show();
+
+        }else if (code.equals("login_false")){
+            alertBuilder.setMessage(message);
+            alertBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    EditText email = activity.findViewById(R.id.email_login);
+                    EditText password = activity.findViewById(R.id.password_login);
+                    email.setText("");
+                    password.setText("");
+                    dialogInterface.dismiss();
+                }
+            });
+
+
+
         }
+
+        AlertDialog alertDialog = alertBuilder.create();
+        alertDialog.show();
 
     }
 }
